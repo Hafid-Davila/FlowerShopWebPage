16c213557454a527d75d733ec3aa855f93b53a78
unknown <hafid.davila@gmail.com>
2023-10-17 22:03:16 -0600

    autom updates

--- public/components/carousel.js
+++ public/components/carousel.js
@@ -1,14 +1,1 @@
-let slideIndex = 0;
-carousel();
 
-function carousel() {
-  let i;
-  let x = document.getElementsByClassName("carousel");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 5000); // Change image every 5 seconds
-}
